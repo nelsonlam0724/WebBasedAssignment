af7e62f16ac62d001f36fb95286933f6893446d6
How <144321866+SuQing0902@users.noreply.github.com>
2024-09-03 19:46:39 +0800

    ..

--- version1/_base.php
+++ version1/_base.php
@@ -1,23 +1,5 @@
 <?php
 
-<<<<<<< Updated upstream
-//PHP Setups
-date_default_timezone_set('Asia/Kuala_Lumpur');
-session_start();
-
-//GETPOST
-function is_get() {
-    return $_SERVER['REQUEST_METHOD'] == 'GET';
-}
-
-
-function is_post() {
-    return $_SERVER['REQUEST_METHOD'] == 'POST';
-}
-
-
-function get($key, $value = null) {
-=======
 // ============================================================================
 // PHP Setups
 // ============================================================================
@@ -44,58 +26,24 @@ function is_post()
 // Obtain GET parameter
 function get($key, $value = null)
 {
->>>>>>> Stashed changes
     $value = $_GET[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
 
-<<<<<<< Updated upstream
-
-function post($key, $value = null) {
-=======
 // Obtain POST parameter
 function post($key, $value = null)
 {
->>>>>>> Stashed changes
     $value = $_POST[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
 
-<<<<<<< Updated upstream
-
-function req($key, $value = null) {
-=======
 // Obtain REQUEST (GET and POST) parameter
 function req($key, $value = null)
 {
->>>>>>> Stashed changes
     $value = $_REQUEST[$key] ?? $value;
     return is_array($value) ? array_map('trim', $value) : trim($value);
 }
 
-<<<<<<< Updated upstream
-function redirect($url = null) {
-    $url ??= $_SERVER['REQUEST_URI'];
-    header("Location: $url");
-    exit();
-}
-
-function temp($key, $value = null) {
-    if($value !== null) {
-        $SEESION["temp_$key"] = $value;
-    } else {
-        $value = $_SESSION["temp_$key"] ?? null;
-        unset($_SESSION["temp_$key"]);
-        return $value;
-    }
-}
-
-//Generate input type
-
-
-//Setup DB
-$_db = new PDO('mysql:dbname=web_ass','root','',[
-=======
 // Redirect to URL
 function redirect($url = null)
 {
@@ -304,15 +252,10 @@ function logout($url = '/')
 
 // Global PDO object
 $_db = new PDO('mysql:dbname=wb', 'root', '', [
->>>>>>> Stashed changes
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
 ]);
 
 
-<<<<<<< Updated upstream
-?>
-=======
 // ============================================================================
 // Global Constants and Variables
 // ============================================================================
->>>>>>> Stashed changes
