f9ec0764e3e5f152167a7b618f948dd45123ac0d
How <144321866+SuQing0902@users.noreply.github.com>
2024-09-03 20:09:20 +0800

    Update loginAdmin.php

--- version1/loginAdmin.php
+++ version1/loginAdmin.php
@@ -1,6 +1,8 @@
 <?php
 include '_base.php';
 
+// ----------------------------------------------------------------------------
+
 if (is_post()) {
     $email = req('email');
     $password = req('password');
@@ -8,7 +10,8 @@ if (is_post()) {
     // Validate: email
     if ($email == '') {
         $_err['email'] = 'Required';
-    } else if (!is_email($email)) {
+    }
+    else if (!is_email($email)) {
         $_err['email'] = 'Invalid email';
     }
 
@@ -20,44 +23,39 @@ if (is_post()) {
     // Login user
     if (!$_err) {
         $stm = $_db->prepare('
-            SELECT * FROM admin
-            WHERE email = ? AND password = ?
-    ');
+            SELECT * FROM user
+            WHERE email = ? AND password = SHA1(?)
+        ');
         $stm->execute([$email, $password]);
         $u = $stm->fetch();
 
         if ($u) {
             temp('info', 'Login successfully');
-            login($u, 'admin/admin.php');
-        } else {
+            login($u);
+        }
+        else {
             $_err['password'] = 'Not matched';
         }
     }
 }
+
+// ----------------------------------------------------------------------------
+
 $_title = 'Login';
+include '_head.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $_title ?></title>
-</head>
+<form method="post" class="form">
+    <label for="email">Email</label>
+    <?= html_text('email', 'maxlength="100"') ?>
+    <?= err('email') ?>
 
-<body>
-    <form method="post" class="form">
-        <label for="email">Email :</label>
-        <?= html_text('email', 'maxlength="100" required type="email"') ?>
-        <?= err('email') ?>
-        <br>
-        <label for="password">Password :</label>
-        <?= html_password('password', 'maxlength="100" required') ?>
-        <?= err('password') ?>
-        <br>
-        <button type="submit">Login</button>
+    <label for="password">Password</label>
+    <?= html_password('password', 'maxlength="100"') ?>
+    <?= err('password') ?>
+
+    <section>
+        <button>Login</button>
         <button type="reset">Reset</button>
-    </form>
-</body>
-
-</html>
+    </section>
+</form>
