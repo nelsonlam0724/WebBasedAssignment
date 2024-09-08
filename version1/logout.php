cacf35f149f0835e9afd4b036d765e60959b6576
How <144321866+SuQing0902@users.noreply.github.com>
2024-09-08 21:45:40 +0800

    remember me and fix backup

--- version1/logout.php
+++ version1/logout.php
@@ -1,10 +1,17 @@
 <?php
 include '_base.php';
-include '_head.php';
-// ----------------------------------------------------------------------------
 
-temp('info', 'Logout successfully');
+// Check if there is a remember me token to clear
+if (isset($_COOKIE['remember_token'])) {
+    // Clear the token in the database
+    $stm = $_db->prepare('UPDATE user SET remember_token = NULL, remember_token_expiry = NULL WHERE remember_token = ?');
+    $stm->execute([$_COOKIE['remember_token']]);
+
+    // Delete the cookie
+    setcookie('remember_token', '', time() - 3600, '/', '', false, true);
+}
+
+session_destroy();
+
+// Redirect to the login page
 logout('login.php');
-
-
-// ----------------------------------------------------------------------------
