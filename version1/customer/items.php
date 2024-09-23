1d45452223d4524c4985443a1436f63ff03747ac
How <144321866+SuQing0902@users.noreply.github.com>
2024-09-23 15:19:40 +0800

    1

--- version1/customer/items.php
+++ version1/customer/items.php
@@ -16,7 +16,7 @@ $productImgs = $getProductImg->fetchAll(PDO::FETCH_OBJ);
 
 $imageProduct = [];
 foreach ($productImgs as $productImg) {
-    $imageProduct[] = $productImg->product_photo;
+  $imageProduct[] = $productImg->product_photo;
 }
 
 $getComment = $_db->prepare('
@@ -37,30 +37,30 @@ $comment = $getComment->fetchAll();
 
 <div class="container">
   <div class="item-board  cards">
-  <div class="items-cards">
-    
-      <img id="images" src="https://www.bing.com/th?id=OIP.CJUZPzHxXG_PeTDV4UuojQHaF0&w=200&h=150&c=8&rs=1&qlt=90&o=6&dpr=1.2&pid=3.1&rm=2" >
+    <div class="items-cards">
+
+      <img id="images" src="https://www.bing.com/th?id=OIP.CJUZPzHxXG_PeTDV4UuojQHaF0&w=200&h=150&c=8&rs=1&qlt=90&o=6&dpr=1.2&pid=3.1&rm=2">
       <div class="btn-field-img">
-      <button class="button_mul_img" data-select="left"><i class="fa fa-angle-double-left" style="font-size:36px"></i></button>
-      <button class="button_mul_img" data-select="right"><i class="fa fa-angle-double-right" style="font-size:36px"></i></button>
+        <button class="button_mul_img" data-select="left"><i class="fa fa-angle-double-left" style="font-size:36px"></i></button>
+        <button class="button_mul_img" data-select="right"><i class="fa fa-angle-double-right" style="font-size:36px"></i></button>
       </div>
-    <p id="demo" style="font-size: 20px;"></p>
+      <p id="demo" style="font-size: 20px;"></p>
     </div>
 
 
     <div class="box2">
-     
+
 
       <div class="product-title"><?= $product->name ?></div>
-      
-    <div class="product-description">
+
+      <div class="product-description">
         <?= $product->description ?>
+      </div>
+
+
+      <div class="price">RM <?= $product->price ?></div>
+      <button class="add-to-card add_card" data-add="<?= $product->product_id ?>">ADD TO CART</button>
     </div>
-  
-   
-    <div class="price">RM <?= $product->price ?></div>
-    <button class="add-to-card add_card" data-add="<?= $product->product_id ?>">ADD TO CART</button>
-    </div>
 
 
 
@@ -70,7 +70,7 @@ $comment = $getComment->fetchAll();
 
 
 
-    
+
   </div>
   <div class="review-field">
     <h3>Reviews</h3>
@@ -88,17 +88,26 @@ $comment = $getComment->fetchAll();
         </div>
         <div class="review-content">
           <?php
+          // Fetch category information
           $getCategory = $_db->prepare('SELECT * FROM category WHERE category_id = ?');
           $getCategory->execute([$c->category_id]);
           $category = $getCategory->fetch();
           ?>
-          <div class="variation">Variation: <?= $category->category_name ?></div>
-          <div class="comment-text"><?= $c->comment ?></div>
+          <div class="variation">Variation: <?= htmlspecialchars($category->category_name) ?></div>
+          <div class="comment-text"><?= htmlspecialchars($c->comment) ?></div>
           <?php if ($c->photo != null) {  ?>
-            <img src="../comment_img/<?= $c->photo ?>" alt="User Image" width="100" height="100">
+            <img src="../comment_img/<?= htmlspecialchars($c->photo) ?>" alt="User Image" width="100" height="100">
           <?php }  ?>
-          <div class="date"><?= $c->datetime ?></div>
+          <div class="date"><?= htmlspecialchars($c->datetime) ?></div>
+
+          <?php if (!empty($c->reply)) { ?>
+            <div class="admin-reply">
+              <span class="reply-header">Admin Reply:</span>
+              <strong><div class="reply-content"><?= htmlspecialchars($c->reply) ?></div></strong>
+            </div>
+          <?php } ?>
         </div>
+
       </div>
     <?php $count++;
     endforeach;
@@ -115,20 +124,20 @@ $comment = $getComment->fetchAll();
 
 <script src="../js/product.js"></script>
 <script>
-const arr =<?php echo json_encode($imageProduct); ?>;
-let i = 0;
-const $image = $('#images');
-const $demo = $('#demo');
-const updateImage = () => {
-  $image.prop('src','../uploads/' + arr[i]);
-  $demo.text('Image ' + (i + 1) + ' of ' + arr.length);
-};
-updateImage();
-$('[data-select]').on('click', e => {
-  const towards = e.target.getAttribute('data-select');
-  i = (towards === 'left') ? (i === 0 ? arr.length - 1 : i - 1) : (i === arr.length - 1 ? 0 : i + 1);
+  const arr = <?php echo json_encode($imageProduct); ?>;
+  let i = 0;
+  const $image = $('#images');
+  const $demo = $('#demo');
+  const updateImage = () => {
+    $image.prop('src', '../uploads/' + arr[i]);
+    $demo.text('Image ' + (i + 1) + ' of ' + arr.length);
+  };
   updateImage();
-});
+  $('[data-select]').on('click', e => {
+    const towards = e.target.getAttribute('data-select');
+    i = (towards === 'left') ? (i === 0 ? arr.length - 1 : i - 1) : (i === arr.length - 1 ? 0 : i + 1);
+    updateImage();
+  });
 </script>
 </body>
 
