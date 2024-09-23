9f7586aed1f4359e180772c1e2cd6a5c53fbc315
waiKitcChong <150654219+waiKitcChong@users.noreply.github.com>
2024-09-23 16:41:16 +0800

    ；

--- version1/function/add_to_cart.php
+++ version1/function/add_to_cart.php
@@ -14,7 +14,7 @@ if ($existingUnit === false) {
     $stm = $_db->prepare('INSERT INTO carts (id,user_id, product_id, unit) VALUES (?,?, ?, ?)');
     $stm->execute([$cartId,$user, $product, "1"]);
 } else {
-
+       
     $stm = $_db->prepare('UPDATE carts SET unit = unit + 1 WHERE product_id = ? AND user_id = ?');
     $stm->execute([$product, $user]);
 }
